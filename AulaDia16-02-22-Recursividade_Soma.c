1a503683fd9fcdb480c8112633499185123ca4d4
PedroBarduni <95556467+PedroBarduni@users.noreply.github.com>
2022-03-21 19:31:15 -0300

    Create AulaDia16-02-22-Recursividade_Soma.c

---
+++ AulaDia16-02-22-Recursividade_Soma.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int soma(int n)
+{
+    if(n == 1)
+        return 1;
+    else
+        return ( n + soma(n-1));
+}
+int main()
+{
+    int n;
+    printf("Digite um inteiro positivo: ");
+    scanf("%d", &n);
+    printf("Soma: %d\n", soma(n));
+    system("pause");
+}
